568471e48bd451bc3a9030a61d1e6df7bc0ba352
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-03-18 20:26:55 +0900

    feat: remove recoverable from user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :recoverable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable
 
   enum gender: { male: 0, female: 1, other: 2 }
 end
