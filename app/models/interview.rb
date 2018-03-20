722e8b7470ff1ebda8b75c45b53c2a4e1528f55a
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-03-20 20:19:40 +0900

    feat: change Interview model and migration.

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,3 +1,5 @@
 class Interview < ApplicationRecord
   belongs_to :user
+
+  enum status: { approved: 0, on_hold: 1, rejected: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318113806) do
+ActiveRecord::Schema.define(version: 20180319085356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interviews", force: :cascade do |t|
+    t.datetime "start_time", default: -> { "now()" }, null: false
+    t.integer "status", limit: 2, default: 2, null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_interviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,4 +42,5 @@ ActiveRecord::Schema.define(version: 20180318113806) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "interviews", "users"
 end
