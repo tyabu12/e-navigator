a9241256328985a2f85967fc1eebe30fda9f37d5
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-03-20 22:53:53 +0900

    feat: change timezone settings.

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,9 @@ module ENavigator
     # デフォルト言語を日本語に
     config.i18n.default_locale = :ja
 
+    # 時刻はOSローカル時間で保存
+    config.active_record.default_timezone = :local
+
     # form_for のエラーが生成する <div class="field_with_errors"></div> を無効化
     # http://guides.rubyonrails.org/configuring.html#configuring-action-view
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
