ede33977603c11f4a476e95c02c57d309b6b8366
Tomohito YABU <tomotomo.y.1212@gmail.com>
2018-04-11 13:22:38 +0900

    refactor: move method to helper.

--- app/helpers/interviews_helper.rb
+++ app/helpers/interviews_helper.rb
@@ -8,4 +8,15 @@ module InterviewsHelper
     @interviews.find { |interview| interview.approved? }
   end
 
+  def get_select_options(selected_interview_id)
+    @interviews.pluck(:start_time, :id).map { |start_time, id| [
+      l(start_time, format: :long), # 面接開始時間
+      :approved, # 承認状態に更新 (値だと文字列になりエラー)
+      {
+        'data-action': user_interview_path(id: id), # data-action に指定した path が action になる
+        selected: id == selected_interview_id ? true : nil # 承認済みの面接日時を初期選択
+      }
+    ]}
+  end
+
 end
